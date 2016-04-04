d678a9ffb12b493c9a6c1bf948c6060d753b912d
Ashish Pandey <ashish.pandey@gmail.com>
2016-04-04 07:43:52 +0000

    added basic light api

---
+++ src/controllers/lwrf_light.js
@@ -0,0 +1,46 @@
+var express = require('express')
+  , router = express.Router({ mergeParams: true })
+  , lights = require('../model/lights');
+  
+var findLight = function(lightId) {
+    return new Promise(function(found, notFound) {
+        var light = lights.find(function(x) { return x.id == lightId });
+        if(light) {
+            found(light);
+        }
+        else {
+            notFound();
+        }
+    }); 
+}
+
+var lightNotFound = function(res) {
+    return function() { res.status(404).json({status: 404, message: 'light not found'}); }
+}
+
+router.get('/', function (req, res) {
+    findLight(req.params.lightId)
+    .then(
+        function(light) { res.json(light); },
+        lightNotFound(res)
+    );
+});
+
+router.get('/level/:level', function(req, res) {
+    findLight(req.params.lightId)
+    .then(
+        function(light) { 
+            res.json({ 
+                level: req.params.level, 
+                light: light.id,
+                status: 'not yet implemented' 
+                
+            });
+        },
+        lightNotFound(res)
+    );
+})
+
+module.exports = router;
+  
+  
---
+++ src/model/lights.js
@@ -0,0 +1,7 @@
+var lightDefinitions = [
+    { name: 'living room back', id: 0, room: 'living room' },
+    { name: 'living room front', id: 1, room: 'living room' },
+    { name: 'living room top', id: 2, room: 'living room' },
+];
+
+module.exports = lightDefinitions;
